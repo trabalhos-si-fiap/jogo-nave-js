d7a1ff2f05767bb4c9c30d0566150b1304278138
Murilo Godoi <godoi.murilo08@gmail.com>
2024-05-24 11:49:47 -0300

    Adicionando elemento do asteroide

--- js/main.js
+++ js/main.js
@@ -1,16 +1,31 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
-const tamanho = 50;
+const numeroAleatorio = (min, max) => {
+    return Math.round(Math.random() * (max - min) + min)
+}
 
-const nave = { x: 200, y: 200 }
+
+const tamanho = 50;
+const nave = { x: 200,  y: 200 }
+const asteroide = {x: numeroAleatorio(0,500),  y: numeroAleatorio(0,500)}
 
 const desenharNave = () => {
     ctx.fillStyle = '#ddd'
     ctx.fillRect(nave.x, nave.y, tamanho, tamanho)
 }
 
-let direcao;
+const desenharAsteroide = () => {
+    ctx.shadowColor = 'blue';
+    ctx.shadowBlur = 50;
+    ctx.fillStyle = 'blue'
+    ctx.fillRect(asteroide.x, asteroide.y, tamanho, tamanho)
+    ctx.shadowBlur = 0
+}
+
+let direcao, loopId;
+
+
 
 const moveNave = () => {
 if (!direcao)return
@@ -27,13 +42,42 @@ if (!direcao)return
     if (direcao == 'cima'){
         nave.y = nave.y - 10
     }
-
+    if (direcao == 'parado') {
+        nave.y = nave.y
+        nave.x= nave.x
+    }
 
 }
 
-setInterval(() => {
-    ctx.clearRect(0,0,900,600)
+const gameloop = () => {
+    clearInterval(loopId)
+    ctx.clearRect(0,0,900,600);
+    moveNave();
+    desenharNave();
+    desenharAsteroide();
+    
+    
+    loopId = setInterval(() => {
+        gameloop();
+    }, 300)
+}
 
-    moveNave()
-    desenharNave()
-},300)
+gameloop();
+
+document.addEventListener("keydown", ({key}) => {
+    console.log(key);
+    if(key == "ArrowRight"){
+        direcao = 'direita'
+    }
+    if(key == "ArrowLeft"){
+        direcao = 'esquerda'
+    }
+    if(key == "ArrowDown"){
+        direcao = 'baixo'
+    }
+    if(key == "ArrowUp"){
+        direcao = 'cima'
+    }
+})
+
+
